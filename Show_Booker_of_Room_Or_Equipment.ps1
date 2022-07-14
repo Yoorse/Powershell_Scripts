33a885b2e6425a5cde64d9b13ceb5608d57c9371
Eerli Semsen <42339518+Yoorse@users.noreply.github.com>
2022-07-14 11:11:38 +0200

    Add files via upload
    Enable to view who has booked equipment or room in Outlook O365.

---
+++ Show_Booker_of_Room_Or_Equipment.ps1
@@ -0,0 +1,49 @@
+﻿##Ask administrator is they have logged in to the Cloud Exchange
+
+Write-Host "Have you connected to Cloud Exchange?"
+
+##if_else statement in lack of solution/laziness for checking for connection status. O for not connected and 1 for already connected
+
+$Connection = Read-Host -Prompt "Enter 0 for not connected or 1 for connected"
+
+if ($Connection -eq 0 ) {
+
+##Connect to Exchange Session
+ 
+Connect-EXOPsession
+
+##Please use the Alias for the room/equipment 
+
+$Mailbox = Read-Host -Prompt "Enter the name of the mailbox with the domain name too.","Example SK-PVC1-MeetingRoom2"
+
+$Calendar = $Mailbox + ":\calendar"
+
+Write-host "Setting the user Default to review the calendar"
+
+##The final one-liner to enable some rights to the user Default.
+ 
+Set-MailboxFolderPermission -Identity $Calendar -User Default -AccessRights reviewer
+
+Write-Host "User who has book the calendar is now visible for" $Calendar
+
+}
+
+else {
+
+
+##The same commands as above. Only you don't have to connect to a session first.
+
+$Mailbox = Read-Host -Prompt "Enter the name of the mailbox with the domain name too.","Example SK-PVC1-MeetingRoom2"
+
+$Calendar = $Mailbox + ":\calendar"
+
+Write-host "Setting the user Default to review the calendar"
+ 
+Set-MailboxFolderPermission -Identity $Calendar -User Default -AccessRights reviewer
+
+Write-Host "User who has book the calendar is now visible for" $Calendar
+
+
+}
+
+Pause
