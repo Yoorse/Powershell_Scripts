17e3b45d0f6abc1e3cf30ea5c75d782a97db447b
Eerli Semsen <42339518+Yoorse@users.noreply.github.com>
2022-07-14 13:19:09 +0200

    Add files via upload
    Enables a mailbox to be remote and resets password for user.

---
+++ Enable_Remote_Mailbox.ps1
@@ -0,0 +1,19 @@
+﻿$Username = Read-Host -Prompt "Enter the username"
+
+$RemoteRoutingAddress = $username + "@domain.mail.on.microsoft.com"
+
+Enable-RemoteMailbox $Username -RemoteRoutingAddresse $RemoteRoutingAddress
+
+$PrimarySMTP = Read-Host -Prompt "Enter the primary STMP"
+
+Set-RemoteMailbox $Username -PrimarySmtpAddress $PrimarySMTP
+
+$NewPwd = Read-Host -AsSecureString -Prompt "Type password for user"
+
+Set-ADAccountPassword -Identity $Username -NewPassword $NewPwd -Reset 
+
+Write-Host "The user is enabled as remotemailbox and password has been reset." 
+
+
+
+
