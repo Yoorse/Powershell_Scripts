16ac85cca8cbfdda1b75cc61540def74ad960e33
Eerli Semsen <42339518+Yoorse@users.noreply.github.com>
2022-07-14 14:05:45 +0200

    Update Enable_Remote_Mailbox.ps1

--- Enable_Remote_Mailbox.ps1
+++ Enable_Remote_Mailbox.ps1
@@ -4,10 +4,6 @@ $RemoteRoutingAddress = $username + "@domain.mail.on.microsoft.com"
 
 Enable-RemoteMailbox $Username -RemoteRoutingAddresse $RemoteRoutingAddress
 
-$PrimarySMTP = Read-Host -Prompt "Enter the primary STMP"
-
-Set-RemoteMailbox $Username -PrimarySmtpAddress $PrimarySMTP
-
 $NewPwd = Read-Host -AsSecureString -Prompt "Type password for user"
 
 Set-ADAccountPassword -Identity $Username -NewPassword $NewPwd -Reset 
