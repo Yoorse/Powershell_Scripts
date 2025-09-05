328b382d0dc0f79af61a1accea5d4922b1774696
Eerli Semsen <42339518+Yoorse@users.noreply.github.com>
2025-09-05 12:37:38 +0200

    Another powershell-script
    Checks after whitespaces in rows.

---
+++ Check_Rows.ps1
@@ -0,0 +1,26 @@
+﻿Add-Type -AssemblyName System.Windows.Forms
+$OpenFileDialog = New-Object System.Windows.Forms.OpenFileDialog
+$OpenFileDialog.Filter = "CSV files (*.csv)|*.csv|All files (*.*)|*.*"
+$OpenFileDialog.Title = "Choose your CSV-File"
+$null = $OpenFileDialog.ShowDialog()
+$csvPath = $OpenFileDialog.FileName
+
+
+###filtrates empty columns. Add or remove columns. Should be enclosed by '' ex: ($_.'apples') 
+$filfiltrering = Import-Csv -Path $csvPath -Delimiter ";" -Encoding UTF8 | Where-Object { 
+    ($_.(insert_column_here) -match '^\s*$') -or 
+    ($_.(insert_column_here) -match '^\s*$') -or
+    ($_.(insert_column_here) -match '^\s*$')
+} 
+    
+
+###Show empty columns or 
+if ($filfiltrering.Count -gt 0) {
+    Write-Host "These rows needs to be filled.:"
+    $filfiltrering | Format-table | Out-Host 
+
+} else {
+    
+    Write-Host -ForegroundColor Green "All rows are filled."
+
+}
